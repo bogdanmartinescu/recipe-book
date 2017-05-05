34cbc951b63f46ffcd5f07632bcb04f3fc82c15c
Bogdan Martinescu <liviu.bogdan.martinescu@gmail.com>
2017-05-05 19:59:17 +0300

    app structure

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes:Recipe[] = [
+    new Recipe('A test recipe', 'This is a test', 'https://static01.nyt.com/images/2015/07/27/dining/27SPAGHETTI/27SPAGHETTI-superJumbo.jpg'),
+    new Recipe('A test recipe', 'This is a test', 'https://static01.nyt.com/images/2015/07/27/dining/27SPAGHETTI/27SPAGHETTI-superJumbo.jpg')
+    
+  ];
 
   constructor() { }
 
